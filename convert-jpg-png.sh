159af7cab12b91119ec2de990b1ddfa5e83090a0
vaykmuniz <vaykzmuniz@protonmail.com>
2022-09-07 17:31:08 -0300

    returns results

--- convert-jpg-png.sh
+++ convert-jpg-png.sh
@@ -2,19 +2,27 @@
 
 cd ~/Documents/MyStuff/learning/shell_script/books_img
 
-##IF DO NOT HAVE PNG FOLDER
-if [ ! -d png ] 
+convert_all_imgs() {
+
+	##IF DO NOT HAVE PNG FOLDER
+	if [ ! -d png ]; then
+		mkdir png
+	fi
+
+	##EVERY .JPG
+	for img in *.jpg; do
+		## REMOVE .jpg from name | split . gets first part
+		local img_file_name=$(ls $img | awk -F. '{ print $1 }')
+		echo converting $img_file_name
+		convert $img_file_name.jpg png/$img_file_name.png
+	done
+	echo the end
+}
+
+convert_all_imgs 2>errors.txt
+if [ $? -eq 0 ]
 then
-	mkdir png
+	echo "Success! :D"
+else
+	echo "Fail D:"
 fi
-
-##EVERY .JPG
-for img in *.jpg
-do 
-	## REMOVE .jpg from name | split . gets first part
-	img_file_name=$(ls $img | awk -F. '{ print $1 }')
-	echo converting $img_file_name
-	convert $img_file_name.jpg png/$img_file_name.png
-done
-
-echo the end

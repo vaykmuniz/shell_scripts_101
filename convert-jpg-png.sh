c8c9411596b5c45b2e22b58d8184f895c4b0cb08
vaykmuniz <vaykzmuniz@protonmail.com>
2022-09-07 13:47:20 -0300

    checks n create new folder png

--- convert-jpg-png.sh
+++ convert-jpg-png.sh
@@ -2,11 +2,19 @@
 
 cd ~/Documents/MyStuff/learning/shell_script/books_img
 
+##IF DO NOT HAVE PNG FOLDER
+if [ ! -d png ] 
+then
+	mkdir png
+fi
+
+##EVERY .JPG
 for img in *.jpg
 do 
+	## REMOVE .jpg from name | split . gets first part
 	img_file_name=$(ls $img | awk -F. '{ print $1 }')
 	echo converting $img_file_name
-	convert $img_file_name.jpg $img_file_name.png
+	convert $img_file_name.jpg png/$img_file_name.png
 done
 
 echo the end
